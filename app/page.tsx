8584e9fc538f31f5251b1dfb838b419231f55853
s2iyeon <s2iyeon.dev@gmail.com>
2026-01-14 07:43:05 +0000

    .env 변수값 출력

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>final-project-sample v02</h1>;
+  return (
+    <>
+      <h1>final-project-sample v02</h1>
+      <h2>client-id: {process.env.NEXT_PUBLIC_CLIENT_ID}</h2>
+    </>
+  );
 }
